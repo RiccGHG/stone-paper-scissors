cd5cbf312f597f265039fc378e334ee3138aa157
RiccGHG <richard10hahn@gmail.com>
2025-12-29 11:41:51 +0000

    Added player 2 Added Menu nuttons after the Game

--- offline/main.js
+++ offline/main.js
@@ -1,7 +1,7 @@
 console.log("Script runs");
 async function start(bot = false) {
-    const h1 = document.querySelector("h1");
-    h1.textContent = "Select your weapon";
+  const h1 = document.querySelector("h1");
+  h1.textContent = "Select your weapon";
   document.body.style.transition = "background 1s ease";
   document.body.style.background = "linear-gradient(135deg, blue, darkblue)";
   const player1 = await putChoices();
@@ -16,8 +16,18 @@ async function start(bot = false) {
     const botImg = `../pictures/${botChoose}.png`;
     const winner = decideWinner(player1, botChoose);
     h1.textContent = "Results!";
-    results(playerImg, botImg, bot, winner)
+    results(playerImg, botImg, bot, winner);
+    return;
   }
+  h1.textContent = "The second player's turn is now up.";
+  await playerChange();
+  h1.textContent = "Select your weapon";
+  const player2 = await putChoices();
+  const p1Img = `../pictures/${player1}.png`;
+  const p2Img = `../pictures/${player2}.png`;
+  const winner = decideWinner(player1, player2);
+  h1.textContent = "Results!";
+  results(p1Img, p2Img, bot, winner);
 }
 async function putChoices() {
   let choose;
@@ -79,50 +89,80 @@ function getOption() {
 
 function decideWinner(player1Choice, player2Choice) {
   if (player1Choice === player2Choice) {
-    return { player1: 'tied', player2: 'tied' };
+    return { player1: "tied", player2: "tied" };
   }
   const beats = {
-    stone: 'scissors',
-    paper: 'stone',
-    scissors: 'paper',
+    stone: "scissors",
+    paper: "stone",
+    scissors: "paper",
   };
   if (beats[player1Choice] === player2Choice) {
-    return { player1: 'win', player2: 'lose' };
+    return { player1: "win", player2: "lose" };
   }
-  return { player1: 'lose', player2: 'win' };
+  return { player1: "lose", player2: "win" };
 }
 /**
- * 
- * @param {String} player1 
- * @param {string} player2 
- * @param {boolean} bot 
- * @param {{player1: string, player2: string}} winnerObj 
+ *
+ * @param {String} player1
+ * @param {string} player2
+ * @param {boolean} bot
+ * @param {{player1: string, player2: string}} winnerObj
  */
 function results(player1, player2, bot, winnerObj) {
-    document.body.style.background = "linear-gradient(135deg, white, grey)"
+  document.body.style.background = "linear-gradient(135deg, white, grey)";
   const modes = document.querySelector(".modes");
+  modes.style.display = "flex";
+  modes.style.flexDirection = "column";
+  const otherOption = bot ? "Play against a person" : "Play against a bot";
   const player2Txt = bot ? "Bot" : "Player 2";
   const player1Color = getColor(winnerObj.player1);
   const player2Color = getColor(winnerObj.player2);
   modes.innerHTML = `
-    <div>
-    <label style="color: ${player1Color}; font-weight: bold;">Player 1</label>
-    <img src="${player1}">
-    </div>
-     <div>
-    <label style="color: ${player2Color}; font-weight: bold;">${player2Txt}</label>
-    <img src="${player2}">
-    </div>
+        <div style="display: flex; gap: 100px; justify-content: center; align-items: center; margin-bottom: 10px;">
+            <div>
+                <label style="color: ${player1Color}; font-weight: bold;">Player 1</label>
+                <img src="${player1}">
+            </div>
+            <div>
+                <label style="color: ${player2Color}; font-weight: bold;">${player2Txt}</label>
+                <img src="${player2}">
+            </div>
+        </div>
+        <div style="display: flex; gap: 20px; justify-content: center; align-items: center; margin-bottom: 10px;">
+            <button onclick="reset(); start(${bot});">Play Again</button>
+            <button onclick="reset(); start(${!bot});">${otherOption}</button>
+            <button onclick="window.location.href='../'">Back</button>
+            </div>
     `;
-    document.querySelectorAll("img").forEach((img) => {
-        img.style.cursor = "default"
-    })
+  document.querySelectorAll(".modes img").forEach((img) => {
+    img.style.cursor = "default";
+  });
 }
 function getColor(resultType) {
-    const obj = {
-        win: "green",
-        tied: "yellow",
-        lose: "red"
-    }
-    return obj[resultType];
+  const obj = {
+    win: "green",
+    tied: "yellow",
+    lose: "red",
+  };
+  return obj[resultType];
+}
+function reset() {
+  const modes = document.querySelector(".modes");
+  modes.style.display = "";
+  modes.style.flexDirection = "";
+}
+async function playerChange() {
+  const modes = document.querySelector(".modes");
+  modes.innerHTML = `
+  <div>
+  <label>Please let player 2 play now.</label>
+  <button id="Change">Ok</button>
+  </div>
+  `;
+  await new Promise((res) => {
+    document
+      .getElementById("Change")
+      .addEventListener("click", () => res(true));
+  });
+  return;
 }
